6fffe5c1c0854558cbe1739c94214997cb927ddd
abel-otegbola <abel15655@gmail.com>
2022-08-27 13:21:04 +0100

    feat: add background image to login and register page

--- src/components/bottombar.js
+++ src/components/bottombar.js
@@ -13,7 +13,7 @@ const MobileBottombar = () => {
 
     return (
         <Flex justify="space-between" align="center" p="5px 10px" bgColor="white" borderColor="gray.100" w="100%" display={[ "flex", "flex", "none" ]} position="fixed" left="0" bottom="0" shadow="dark-lg">
-            <Link href="/login" p="15px 20px" fontSize="20px" borderRadius="5px" _hover={{ bgColor: "orange.300", color: "white" }} style={ (location === "/login")? active : { color: ""} }><FiUser /></Link>
+            <Link href="/login" p="15px 20px" fontSize="20px" borderRadius="5px" _hover={{ bgColor: "orange.300", color: "white" }} style={ (location === "/login" || "/login" || "dashboard" || "/admin")? active : { color: ""} }><FiUser /></Link>
             <Link href="/shop" p="15px 20px" fontSize="20px" borderRadius="5px" _hover={{ bgColor: "orange.300", color: "white" }} style={ (location === "/shop")? active  : { color: "" } }><FiShoppingCart /></Link>
             <Link href="/" p="15px 20px" fontSize="20px" borderRadius="5px" _hover={{ bgColor: "orange.300", color: "white" }} style={ (location === "/")? active : { color: ""} }><FiHome /></Link>
             <Link href="/" p="15px 20px" fontSize="20px" borderRadius="5px" _hover={{ bgColor: "orange.300", color: "white" }} style={ (location === "/notifications")? active : { color: ""}}><FiBell /></Link>
--- src/views/register.js
+++ src/views/register.js
@@ -1,21 +1,23 @@
 import { Box, Flex, Text, Heading, FormControl, FormLabel, Input, Link, Button } from "@chakra-ui/react"
 import { useState } from "react";
 import { FaEnvelope, FaEye, FaLock, FaUser } from "react-icons/fa";
+import bg from "../assets/imgs/bg.jpg"
 
 const Register = () => {
     const [ type, setType ] = useState(true)
     const [ cpasswordtype, setCpasswordtype ] = useState(true)
 
     return (
-        <Flex m="50px 10%" justify="center" fontSize="14px">
-            <Box>
+        <Flex m="20px" justify="center" fontSize="14px" backgroundImage={`url(${bg})`} bgSize="cover">
+            <Flex justify="center" w="100%" bgColor="whiteAlpha.500">
+            <Box p="20px" w={[ "100%", "400px", "500px" ]} m="20px" bgColor="white">
                 <Heading textAlign="center">Register!</Heading>
                 <Text textAlign="center" fontWeight="600" py="3">Fill in your details to signup.</Text>
 
                 <FormControl mt="4">
                     <FormLabel fontSize="14px">Full name </FormLabel>
                     
-                    <Flex align="center" w={[ "100%", "300px" ]} p="2px" border="1px" borderColor="gray.100" borderRadius="0">
+                    <Flex align="center" w="100%" p="2px" border="1px" borderColor="gray.100" borderRadius="0">
                         <Button href="/" bgColor="gray.100" p="3" border="1px" borderRadius="0" borderColor="gray.100">
                             <FaUser />
                         </Button>
@@ -26,7 +28,7 @@ const Register = () => {
                 <FormControl mt="4">
                     <FormLabel fontSize="14px">Email address </FormLabel>
                     
-                    <Flex align="center" w={[ "100%", "300px" ]} p="2px" border="1px" borderColor="gray.100" borderRadius="0">
+                    <Flex align="center" w="100%" p="2px" border="1px" borderColor="gray.100" borderRadius="0">
                         <Button href="/" bgColor="gray.100" p="3" border="1px" borderRadius="0" borderColor="gray.100">
                             <FaEnvelope />
                         </Button>
@@ -37,7 +39,7 @@ const Register = () => {
                 <FormControl mt="4">
                     <FormLabel fontSize="14px">Password </FormLabel>
                     
-                    <Flex align="center" w={[ "100%", "300px" ]} p="2px" border="1px" borderColor="gray.100" borderRadius="0">
+                    <Flex align="center" w="100%" p="2px" border="1px" borderColor="gray.100" borderRadius="0">
                         <Button href="/" bgColor="gray.100" p="3" border="1px" borderRadius="0" borderColor="gray.100">
                             <FaLock />
                         </Button>
@@ -51,7 +53,7 @@ const Register = () => {
                 <FormControl mt="4">
                     <FormLabel fontSize="14px">Confirm Password </FormLabel>
                     
-                    <Flex align="center" w={[ "100%", "300px" ]} p="2px" border="1px" borderColor="gray.100" borderRadius="0">
+                    <Flex align="center" w="100%" p="2px" border="1px" borderColor="gray.100" borderRadius="0">
                         <Button href="/" bgColor="gray.100" p="3" border="1px" borderRadius="0" borderColor="gray.100">
                             <FaLock />
                         </Button>
@@ -66,6 +68,7 @@ const Register = () => {
 
                 <Text mt="4">Already have an account? <Link href="/login" color="orange.300">Login</Link></Text>
             </Box>
+            </Flex>
         </Flex>
     )
 }
