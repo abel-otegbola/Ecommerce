807e11f8f48db1d0b05e8eccea7e6f7772201eed
abel-otegbola <abel15655@gmail.com>
2022-09-04 06:55:39 +0100

    feat: add typeahead functionality to search

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,14 +1,40 @@
-import { Flex, Input, Link } from "@chakra-ui/react";
+import { Box, Flex, Input, Link } from "@chakra-ui/react";
+import { useState } from "react";
 import { FaSearch } from "react-icons/fa";
+import { useSelector } from "react-redux";
+import { Link as SearchLink } from "react-router-dom";
 
 const SearchBar = () => {
+    const products = useSelector((state) => state.data.products)
+    const [query, setQuery] = useState("")
+
+
     return (
 
-        <Flex align="center" w="100%" p="2px" border="1px" borderColor="gray.100" borderRadius="0">
-            <Input placeholder="Search here" fontSize="14px" borderRadius="0" />
+        <Flex 
+            align="center" 
+            w="100%" p="2px" 
+            border="1px" 
+            borderColor="gray.100" 
+            borderRadius="0"
+            position="relative"
+        >
+            <Input placeholder="Search here" fontSize="14px" borderRadius="0" onChange={(e) => setQuery(e.target.value)} />
             <Link href="/" bgColor="gray.100" p="3" border="1px" borderColor="gray.100">
                 <FaSearch />
             </Link>
+
+            <Box position="absolute" top="100%" left="0" w="100%" p="2" bgColor="white" shadow="sm" zIndex="10000">
+                {
+                    products && products.filter(product => ( query !== "" && product.title.toUpperCase().indexOf(query.toUpperCase()) !== -1)).map(item => {
+                        return (
+                            <Box p="2" key={item.id}>
+                                <SearchLink to={`/singleProduct?id=${item.id}`}>{item.title}</SearchLink>
+                            </Box>
+                        )
+                    })
+                }
+            </Box>
         </Flex>
 
     )
