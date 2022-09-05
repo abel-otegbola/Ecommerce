f33716f5b10b665e1a92a3630709cab996cb1f7f
abel-otegbola <abel15655@gmail.com>
2022-09-05 17:20:36 +0100

    fix: increase herosection button padding

--- src/components/heroSection.js
+++ src/components/heroSection.js
@@ -12,7 +12,8 @@ const HeroSection = ({ mainText, subText }) => {
             <Text fontSize="40px" lineHeight="50px" fontWeight="700" pb="15px" >{mainText}</Text>
             <Text fontSize="14px" lineHeight="30px" fontWeight="600" mb="35px">{subText}</Text>
             <Link href="/shop" 
-                p="10px 20px" 
+                p="15px 25px" 
+                borderRadius="2px"
                 bgColor="brand.900" 
                 color="white" 
                 _hover={{ bgColor: "brand.800", color: "white" }}>
