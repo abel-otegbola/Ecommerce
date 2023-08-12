ff1aeb8f2858fb0acd10ea39fd544420dca89d2e
abel-otegbola <abel15655@gmail.com>
2023-08-12 01:14:04 +0100

    feat: add dashboard, order, account and notification tabs

--- src/views/dashboard/dashboard.jsx
+++ src/views/dashboard/dashboard.jsx
@@ -1,12 +1,34 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Link, Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react";
 import { useSelector } from "react-redux";
 
 const Dashboard = () => {
     const user = useSelector(state => state.data.user)
 
     return (
-        <Flex>
-            {user.email}
+        <Flex px={[ null, "20px", "5%", "10%" ]} py="5%" minH={"90vh"}>
+            <Tabs>
+                <TabList>
+                    {
+                        ["Dashboard", "Orders", "Account", "Notifications"].map((tab, i) => (
+                            <Tab key={i}>{tab}</Tab>
+                        ))
+                    }
+                </TabList>
+
+                <TabPanels>
+                    <TabPanel>
+                    <p>Welcome: {user.email}</p>
+                    </TabPanel>
+
+                    <TabPanel>
+                    <p>You have not placed any order. Visit the <Link color={"green.500"} href="/Shop">shop</Link> to add item to cart and place order.</p>
+                    </TabPanel>
+
+                    <TabPanel>
+                    <p>three!</p>
+                    </TabPanel>
+                </TabPanels>
+                </Tabs>
         </Flex>
     )
 }
